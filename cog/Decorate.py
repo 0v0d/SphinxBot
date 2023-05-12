7f3d9aeb94dbb78efbc9f984b94c1e07c2dea430
0v0d <86046148+0v0d@users.noreply.github.com>
2023-05-12 20:14:59 +0900

    fixed

--- cog/Decorate.py
+++ cog/Decorate.py
@@ -7,6 +7,7 @@ class DecorateText(commands.Cog):
         self.bot = bot
 
     @commands.slash_command()
-    async def decorate_code(self, ctx, language: discord.Option(str, choices=['py', 'cpp']), code: str):
+    async def decorate_code(self, ctx, language: discord.Option(str,
+                                                                choices=['py', 'cpp', 'cs', 'java', 'cs', 'ts', 'js',
+                                                                         'md', 'diff']), code: str):
         await ctx.respond(f'```{language}\n{code}\n```')
-
