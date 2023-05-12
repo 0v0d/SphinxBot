fa27fa22e88ba4b6e96d28bb6a4a2b422f5e9094
0v0d <86046148+0v0d@users.noreply.github.com>
2023-05-12 10:27:44 +0000

    Remove env

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 import discord
-from dotenv import load_dotenv
 
 from Keep_Alive import keep_alive
 from cog.Decorate import DecorateText
@@ -14,10 +13,10 @@ bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print("Started")
-    print(bot.user.name)
-    print(bot.user.id)
-    await bot.change_presence(activity=discord.Game(name="im sleepy"))
+  print("Started")
+  print(bot.user.name)
+  print(bot.user.id)
+  await bot.change_presence(activity=discord.Game(name="im sleepy"))
 
 
 bot.add_cog(Random(bot))
@@ -25,5 +24,4 @@ bot.add_cog(Calculator(bot))
 bot.add_cog(Weather(bot))
 bot.add_cog(DecorateText(bot))
 keep_alive()
-load_dotenv()
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
