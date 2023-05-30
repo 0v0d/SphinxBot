159e1c60dae58daf530817d354a4278cc9dfabc7
0v0d <86046148+0v0d@users.noreply.github.com>
2023-05-30 11:02:00 +0900

    fixed weather

--- cog/Weather.py
+++ cog/Weather.py
@@ -1,12 +1,12 @@
 import os
-
+import requests
 import discord
-import requests as requests
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.environ.get("API_KEY")
+url = "http://api.openweathermap.org/data/2.5/"
 
 
 class Weather(commands.Cog):
@@ -15,13 +15,15 @@ class Weather(commands.Cog):
 
     @commands.slash_command(description="現在の天気情報")
     async def weather_now(self, ctx, location):
-        weather = get_weather(str(location) + ',JP')
-        if weather:
-            response = '{} 現在の天気:{} 気温:{}度'.format(location, weather['weather'][0]['description']
-                                                          , weather['main']['temp'])
+        weather_data = get_weather(str(location) + ',JP')
+        if weather_data:
+            weather = weather_data['weather'][0]['description']
+            temp = weather_data['main']['temp']
+            embed = discord.Embed(title=f"{location}の天気", color=discord.Color.blue())
+            embed.add_field(name="現在", value=f"天気: {weather} / 気温: {temp}℃", inline=False)
+            await ctx.respond(embed=embed)
         else:
-            response = f'{location}の天気を取得できませんでした。'
-        await ctx.respond(response)
+            await ctx.respond(f'{location}の天気を取得できませんでした。')
 
     @commands.slash_command(description="4日間の天気情報")
     async def weather_daily(self, ctx, location):
@@ -31,16 +33,16 @@ class Weather(commands.Cog):
             embed = discord.Embed(title=f"{location}の天気予報", color=discord.Color.blue())
             for data in weather_data:
                 time = data['time']
-                temperature = data['temperature']
+                temp = data['temperature']
                 weather = data['weather']
-                embed.add_field(name=time, value=f"天気: {weather} / 気温: {temperature}℃", inline=False)
+                embed.add_field(name=time, value=f"天気: {weather} / 気温: {temp}℃", inline=False)
             await ctx.respond(embed=embed)
         else:
             await ctx.respond('天気情報を取得できませんでした。')
 
 
 def get_weather(location):
-    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={API_KEY}')
+    response = requests.get(f'{url}weather?q={location}&units=metric&appid={API_KEY}')
     if response.status_code == 200:
         return response.json()
     else:
@@ -49,7 +51,7 @@ def get_weather(location):
 
 
 def get_daily_weather(location):
-    response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast?q={location}&units=metric&appid={API_KEY}")
+    response = requests.get(f"{url}forecast?q={location}&units=metric&appid={API_KEY}")
     if response.status_code == 200:
         data = response.json()
         weather_data = []
@@ -61,7 +63,7 @@ def get_daily_weather(location):
             weather_data.append({
                 'time': time,
                 'temperature': temperature,
-                'weather': weather })
+                'weather': weather})
         return weather_data
     else:
         print('エラー')
